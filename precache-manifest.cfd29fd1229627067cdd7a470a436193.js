9bd9f85904802da1f69cb2574e222dbc4f7a7802
keriwisnu <keriwisnu@gmail.com>
2020-01-22 10:38:20 +0000

    Updates

---
+++ precache-manifest.cfd29fd1229627067cdd7a470a436193.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919ce2f9e8c5132bf94539f9ec39f6a1",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "b1d67c26ee10c6f41165",
+    "url": "/contactmanager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "170f70f2142b33913aed",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b1d67c26ee10c6f41165",
+    "url": "/contactmanager/static/js/2.8ecb8ba3.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.8ecb8ba3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "170f70f2142b33913aed",
+    "url": "/contactmanager/static/js/main.66bf521a.chunk.js"
+  },
+  {
+    "revision": "6d5470f28a9dc2aa0ea3",
+    "url": "/contactmanager/static/js/runtime-main.fef8aa86.js"
+  }
+]);
